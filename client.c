7c11a8c3faf89dbbe0a90562fac32d893de32f3b
Aditya Tumarada <adity170123054@iitg.ac.in>
2020-03-11 22:46:48 +0530

    Add files via upload

--- client.c
+++ client.c
@@ -7,6 +7,287 @@
 #include <netdb.h>
 #include <unistd.h>
 
+int login_handler(int socket)
+{
+	char buffer[1024],user_name[512],user_pass[512];
+	int n;
+
+	memset(buffer,0,1024);
+	n = read(socket,buffer,1025);
+
+	if(n < 0)
+		fprintf(stderr, "ERROR reading from socket\n");
+	else
+	if(n==0)
+	{
+		printf("Server Closed\n");             
+		exit(0);
+	}	
+	printf("%s\n",buffer);
+	
+	if(strcmp(buffer,"Number of connections exceeded")==0) 
+		exit(0);
+
+	while(1)
+	{           
+		memset(user_name,0,512);
+		memset(user_pass,0,512);
+		memset(buffer,0,1024);
+		
+		// storing the username provided by user into user_name
+		printf("Enter username:"); 
+		gets(user_name);				
+		user_name[strlen(user_name)] = '\0';
+
+		//storing password into user_pass		
+		printf("Enter password:");
+		gets(user_pass);             
+		user_pass[strlen(user_pass)] = '\0';
+		
+		//we send data to server using buffer.
+		//input format is L#username-passwd
+		strcat(buffer,"L#");              	
+		strcat(buffer,user_name);	
+		strcat(buffer,"-");
+		strcat(buffer,user_pass);
+		strcat(buffer,"\0");
+		
+		//we write this into newsock we got after connecting
+		n=write(socket,buffer,strlen(buffer));		
+		bzero(buffer,1025);
+		n = read(socket,buffer,1025);
+		
+		//if n<0 then error in reading from socket
+		if(n < 0)
+			fprintf(stderr, "ERROR reading from socket\n"); 
+		
+		//if n==0 server has closed so we close the client
+		if(n==0)
+		{
+			printf("Server Closed\n");               
+			exit(0);
+		}
+		printf("%s\n",buffer);
+		
+		//if alreading logged we close the client
+		int v=strcmp(buffer,"Already Logged In\0"); 
+		if(v==0)
+		{
+			exit(0);
+		}
+		int h=strcmp(buffer,"Invalid username or password\0"); 
+		if(h!=0)
+		{
+			break;
+		}
+		bzero(buffer,1025);
+	}
+
+
+	return 0;
+}
+
+
+
+void buy(int newsock)
+{
+	char buffer[1025];
+		int n,code,qty,price;
+		//taking item code , quantity , price 
+		printf("Enter item code (choose items between 1-10) :");
+		while(1)
+		{
+			scanf("%d",&code);
+			if(code>0 && code<=10)  
+			{
+				break;
+			}
+			printf("Enter valid item code :");
+		}
+		printf("Enter item quantity:");
+		while(1)
+		{
+			scanf("%d",&qty);
+			if(qty>0)
+			{
+				break;
+			}
+			printf("Enter valid item quantity :");
+		}
+		printf("Enter item price:");
+		while(1)
+		{
+			scanf("%d",&price);
+			if(price>=0)
+			{
+				break;
+			}
+			printf("Enter valid price :");
+		}
+
+		// format is B#itemcode-quantity-price
+		sprintf(buffer,"B#%d-%d-%d;",code,qty,price);
+		n=write(newsock,buffer,strlen(buffer));
+		if(n < 0){
+			fprintf(stderr, "Error in writing to socket\n");
+			exit(0);
+		}
+		bzero(buffer,1024);
+		n=read(newsock,buffer,1024);
+		if(n<0)
+		{
+			fprintf(stderr, "Error in reading from socket\n");
+			exit(0);
+		}
+		if(n==0)
+		{
+			printf("Server Closed\n");	// if n is 0, server is closed so the client program is closed
+			exit(0);
+		}
+		printf("%s",buffer);
+}
+
+void sell(int newsock)
+{
+		char buffer[1025];
+		//taking item code , quantity , price 
+		int n,code,qty,price;
+		printf("Enter item code (choose items between 1-10) :");
+		while(1)
+		{
+			scanf("%d",&code);
+			if(code>0 && code<=10)   
+			{
+				break;
+			}
+			printf("Enter valid item code :");
+		}
+		printf("Enter item quantity:");
+		while(1)
+		{
+			scanf("%d",&qty);
+			if(qty>0)
+			{
+				break;
+			}
+			printf("Enter valid item quantity :");
+		}
+		printf("Enter item price:");
+		while(1)
+		{
+			scanf("%d",&price);
+			if(price>=0)
+			{
+				break;
+			}
+			printf("Enter valid price :");
+		}
+		// format is S#itemcode-quantity-price
+		sprintf(buffer,"S#%d-%d-%d;",code,qty,price);
+		n=write(newsock,buffer,strlen(buffer));
+		if(n < 0){
+			fprintf(stderr, "Error in writing to socket\n");
+			exit(0);
+		}
+		bzero(buffer,1024);
+		n=read(newsock,buffer,1024);
+		if(n<0)
+		{
+			fprintf(stderr, "Error in reading from socket\n");
+			exit(0);
+		}
+		if(n==0)
+		{
+			printf("Server Closed\n");	// if n is 0, server is closed so the client program is closed
+			exit(0);
+		}
+		printf("%s",buffer);
+}
+
+void order_status(int newsock)
+{
+
+		int n=0;   
+		char buffer[1024];
+		// format is O#
+		n=write(newsock,"O#",2);
+		if(n<0)
+		{
+			fprintf(stderr, "Error in writing to socket\n");
+			exit(0);
+		}
+		bzero(buffer,1024);
+		n=read(newsock,buffer,1024);
+		if(n<0)
+		{
+			fprintf(stderr, "Error in reading from socket\n");
+			exit(0);
+		}
+		if(n==0)
+		{
+			printf("Server Closed\n");// if n is 0, server is closed so the client program is closed
+			exit(0);
+		}
+		printf("%s",buffer);
+}
+
+
+void trade_status(int newsock)
+{
+	
+		int n=0;           
+		char buffer[1024];
+		// format is T#
+		n=write(newsock,"T#",2);
+		if(n<0)
+		{
+			fprintf(stderr, "Error in writing to socket\n");
+			exit(0);
+		}
+		bzero(buffer,1024);
+		n=read(newsock,buffer,1024);
+		if(n<0)
+		{
+			fprintf(stderr, "Error in reading from socket\n");
+			exit(0);
+		}
+		if(n==0)
+		{
+			printf("Server Closed\n");// if n is 0, server is closed so the client program is closed
+			exit(0);
+		}
+		printf("%s",buffer);
+}
+
+void request_handler(int sock)
+{
+	printf("To buy press 1\n");
+	printf("To sell press 2\n");
+	printf("To view order status press 3\n");
+	printf("To view your trade status press 4\n");
+	printf("To exit press 5\n");
+	int n = 0;
+	//calls the respective function based on user request.
+	while(1)
+	{
+		printf("Enter your option:");
+		scanf("%d",&n);
+		switch(n)
+		{
+			case 1: buy(sock);
+					break;
+			case 2: sell(sock);
+					break;
+			case 3: order_status(sock);
+					break;
+			case 4: trade_status(sock);
+					break;
+			case 5: return;
+			default:printf("enter valid option \n"); 
+		}
+	}	
+
+}
    
 int main(int argc, char const *argv[]) 
 { 
@@ -18,7 +299,7 @@ int main(int argc, char const *argv[])
 
 	if(argc!=3)
 	{
-		fprintf(stderr, "input format executable code IP Address Port No.\n" );
+		fprintf(stderr, "input format executable_code IP_Address Port_No.\n" );
 		exit(0);
 	}
 
@@ -49,81 +330,8 @@ int main(int argc, char const *argv[])
 		exit(0);
 	}
 
-	login_handler(sock);
-	printf("hey boi how to doshafijh\n");	
-
-
+	login_handler(sock); //login handler takes care of login part
+	request_handler(sock); //request_handler takes care of user requests
     return 0; 
 }
-
-int login_handler(int socket)
-{
-	char buffer[1024],temp1[512],temp2[512];
-	int n;
-
-	memset(buffer,0,1024);
-	n = read(socket,buffer,1025);
-
-	if(n < 0)
-		fprintf(stderr, "ERROR reading from socket\n");
-	else
-	if(n==0)
-	{
-		printf("Server Closed\n");             
-		exit(0);
-	}	
-
-	printf("%s\n",buffer);
-	if(strcmp(buffer,"Connections Exceeded")==0) 
-		exit(0);
-
-	while(1)
-	{           
-		memset(temp1,0,512);
-		memset(temp2,0,512);
-		memset(buffer,0,1024);
-
-		printf("Enter username:"); 
-		gets(temp1);				// storing the username provided by user into temp1
-		temp1[strlen(temp1)-1] = '\0';
-		printf("Enter password:");
-		gets(temp2);             //storing password into temp2
-		temp2[strlen(temp2)-1] = '\0';
-		strcat(buffer,"L#");              	//in buffer initially we put 1# because this is login information server understands that this login information by seeing this 1
-		strcat(buffer,temp1);				//next we put username:passwd into the buffer which the same format client login detils are stored in login.txt in the server side
-		strcat(buffer,":");
-		strcat(buffer,temp2);
-		strcat(buffer,";\0");
-		n=write(socket,buffer,strlen(buffer));		//we write this into newsock we got after connecting
-		bzero(buffer,1025);
-		n = read(socket,buffer,1025);
-		if(n < 0)
-			fprintf(stderr, "ERROR reading from socket\n"); 
-		if(n==0)
-		{
-			printf("Server Closed\n");                //if n==0 bytes then it is an indication that server has closed so we close the client also
-			exit(0);
-		}
-		printf("%s\n",buffer);
-		
-		int v=strcmp(buffer,"Already Logged In\0");  //if alreading logged in aslo we should close the client
-		
-		if(v==0)
-		{
-			exit(0);
-		}
-		int h=strcmp(buffer,"Invalid Username or Password\0"); //if invalid username or password we do not close the client we print try again and this goes in a loop
-		if(h!=0)
-		{
-			break;
-		}
-		bzero(buffer,1025);
-	}
-
-
-	return 0;
-}
-
-
-
 
--- server.c
+++ server.c
@@ -1,33 +1,298 @@
 #include <stdio.h>  
-#include <string.h>   //strlen  
+#include <string.h>  
 #include <stdlib.h>  
 #include <errno.h>  
-#include <unistd.h>   //close  
-#include <arpa/inet.h>    //close  
+#include <unistd.h>  
+#include <arpa/inet.h>   
 #include <sys/types.h>  
 #include <sys/socket.h>  
 #include <netinet/in.h>  
-#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros  
+#include <sys/time.h> 
+
+#define INT_MAX 2147483647
+
+// structure for request
+typedef struct request
+{
+    int user_id;
+    int item_code;
+    int price;
+    int quantity;
+}request; 
+
+//array of buy requests
+request buy[1024];
+
+//array of sell requests
+request sell[1024];
+
+//structure for matched requests.
+typedef struct orders
+{
+    int sell_id;
+    int buy_id;
+    int price;
+    int quantity;
+    int item_code;
+}orders;
+
+//array of matched orders
+orders trade[1024];
+int index_tr=0;
+
+//helper function for match_trade which return min selling price for 10 items.
+request* trade_helper()
+{
+    request min_sell[10];
+    request * temp;
+    temp = (request *) malloc(10*sizeof(request)); 
+    for(int i=0;i<10;i++)
+    {
+        
+        min_sell[i].price=INT_MAX;
+        min_sell[i].item_code=i+1;
+        min_sell[i].quantity=0;
+        min_sell[i].user_id=-1; 
+    }
+
+    for(int i=0;i<1024;i++)
+    {
+        if(sell[i].price < min_sell[sell[i].item_code-1].price)
+        {
+            min_sell[sell[i].item_code-1]=sell[i];
+        }
+    }
+    
+    for(int i=0;i<10;i++)
+    {
+  
+        if(min_sell[i].price==INT_MAX)
+        {
+            temp[i].item_code=i+1;
+            temp[i].quantity=0;
+            temp[i].price=0;
+        }
+        else
+        {
+            temp[i]=min_sell[i];   
+        }
+
+    } 
+    return temp;
+}
+
+// function which matches sell requests and buy requests.
+// for each buy request function finds min sell price and add both to trade array and makes necessary changes to buy and sell array.
+void match_trade()
+{
+    for(int i=0;i<1024;i++)
+    {
+        if(buy[i].item_code==-1)
+            break;
+
+        request* min_sell=trade_helper();   
+        int itemcode = buy[i].item_code;
+        
+        int flag=1;
+        if(min_sell[itemcode-1].quantity==0)
+        {
+            flag=0;
+        }
+        int k=0;
+
+        for (int j = 0; j < 1024 && flag==1; ++j)
+        {
+            if(min_sell[itemcode-1].user_id==sell[j].user_id && min_sell[itemcode-1].item_code == sell[j].item_code && min_sell[itemcode-1].quantity == sell[j].quantity && min_sell[itemcode-1].price == sell[j].price)
+            {   k=j;
+                break;
+            }
+        }
+
+        if(min_sell[itemcode-1].quantity==buy[i].quantity && flag!=0)
+        {
+            trade[index_tr].sell_id=min_sell[itemcode-1].user_id;
+            trade[index_tr].buy_id=buy[i].user_id;
+            trade[index_tr].price=min_sell[itemcode-1].price;
+            trade[index_tr].quantity=buy[i].quantity;
+            trade[index_tr].item_code=buy[i].item_code;
+            index_tr++;
+            buy[i].quantity=0;
+            buy[i].item_code=0;
+            buy[i].price=0;
+            buy[i].user_id=0;
+            sell[k].quantity=0;
+            sell[k].item_code=0;
+            sell[k].price=0;
+            sell[k].user_id=0;
+        }
+        else
+        if(min_sell[itemcode-1].quantity>buy[i].quantity && flag!=0)
+        {
+            trade[index_tr].sell_id=min_sell[itemcode-1].user_id;
+            trade[index_tr].buy_id=buy[i].user_id;
+            trade[index_tr].price=min_sell[itemcode-1].price;
+            trade[index_tr].quantity=buy[i].quantity;
+            trade[index_tr].item_code=buy[i].item_code;
+            index_tr++;
+            sell[k].quantity=sell[k].quantity-buy[i].quantity;
+            buy[i].quantity=0;
+            buy[i].item_code=0;
+            buy[i].price=0;
+            buy[i].user_id=0;
+            
+        }
+        else
+        if(min_sell[itemcode-1].quantity<buy[i].quantity && flag!=0)
+        {
+            trade[index_tr].sell_id=min_sell[itemcode-1].user_id;
+            trade[index_tr].buy_id=buy[i].user_id;
+            trade[index_tr].price=min_sell[itemcode-1].price;
+            trade[index_tr].quantity=buy[i].quantity;
+            trade[index_tr].item_code=buy[i].item_code;
+            index_tr++;
+            buy[i].quantity=buy[i].quantity-sell[k].quantity;
+            sell[k].quantity=0;
+            sell[k].item_code=0;
+            sell[k].price=0;
+            sell[k].user_id=0;
+            i=i-1;
+        }
+
+    }
+}
 
 
+
+//order status function finds maximum selling price and minimum buying price for each order.
 void orders_st(int st)
 {
+    request max_buy[10];
+    request min_sell[10];
+    char buffer[1024],temp1[512],temp2[512];
+    memset(buffer,0,1024);
+    memset(temp1,0,512);
+    memset(temp2,0,512);
 
+    for(int i=0;i<10;i++)
+    {
+        max_buy[i].price=-1;
+        min_sell[i].price=INT_MAX;
+        max_buy[i].item_code=i+1;
+        max_buy[i].quantity=0;
+        max_buy[i].user_id=-1;  
+        min_sell[i].item_code=i+1;
+        min_sell[i].quantity=0;
+        min_sell[i].user_id=-1; 
+    }
+
+    for(int i=0;i<1024;i++)
+    {
+      
+        if(buy[i].price > max_buy[buy[i].item_code-1].price)
+        {
+            max_buy[buy[i].item_code-1]=buy[i];
+          
+        }
+    }
+
+    for(int i=0;i<1024;i++)
+    {
+        if(sell[i].price < min_sell[sell[i].item_code-1].price)
+        {
+            min_sell[sell[i].item_code-1]=sell[i];
+        }
+    }
+    strcat(buffer,"maximum buying price\n");
+    for(int i=0;i<10;i++)
+    {
+        memset(temp1,0,512);
+        if(max_buy[i].price==-1)
+        {
+            sprintf(temp1,"item %d:price %d:quantity %d\n",i+1,0,0);
+        }
+        else
+        {
+         sprintf(temp1,"item %d:price %d:quantity %d\n",i+1,max_buy[i].price,max_buy[i].quantity);   
+        }
+        strcat(buffer,temp1);
+    }
+    strcat(buffer,"min selling price\n");
+    for(int i=0;i<10;i++)
+    {
+        memset(temp2,0,512);
+        if(min_sell[i].price==INT_MAX)
+        {
+            sprintf(temp2,"item %d:price %d:quantity %d\n",i+1,0,0);
+        }
+        else
+        {
+         sprintf(temp2,"item %d:price %d:quantity %d\n",i+1,min_sell[i].price,min_sell[i].quantity);   
+        }
+        strcat(buffer,temp2);
+    }
+    int n = write(st,buffer,sizeof(buffer));
 }
 
 void trade_st(int st, int user_id)
 {
+    int i=0;
+    char buffer[1024],temp[512];
+    memset(buffer,0,1024);
+
+    while(i<index_tr)
+    {
+        memset(temp,0,512);
+        sprintf(temp,"buy id :%d  sell id :%d item :%d quantity :%d price :%d\n",trade[i].buy_id,trade[i].sell_id,trade[i].item_code,trade[i].quantity,trade[i].price);
+        strcat(buffer,temp);
+        i++;
+    }
+    int n=write(st,buffer,sizeof(buffer));
 
 }
 
-void sell_order(char * t, int user_id)
+void sell_order(char * temp, int user_id)
 {
-
+    char *t=strtok(temp,"-");
+    int itemcode=atoi(t);
+    t=strtok(NULL,"-");
+    int quantity=atoi(t);
+    t=strtok(NULL,"-");
+    int price=atoi(t);
+    int i=0;
+    while(1)
+    {
+        if(sell[i].item_code==-1)
+            break;
+        i++;
+    }
+    
+    sell[i].item_code=itemcode;
+    sell[i].quantity=quantity;
+    sell[i].price=price;
+    sell[i].user_id=user_id;
 }
 
-void buy_order(char * t, int user_id)
+void buy_order(char * temp, int user_id)
 {
+    char *t=strtok(temp,"-");
+    int itemcode=atoi(t);
+    t=strtok(NULL,"-");
+    int quantity=atoi(t);
+    t=strtok(NULL,"-");
+    int price=atoi(t);
+    int i=0;
+    
+    while(1)
+    {
+        if(buy[i].item_code==-1)
+            break;
+        i++;
+    }
 
+    buy[i].item_code=itemcode;
+    buy[i].quantity=quantity;
+    buy[i].price=price;
+    buy[i].user_id=user_id;
 }
 
 int search_index(char buffer[])
@@ -42,11 +307,9 @@ int search_index(char buffer[])
 }
 
   
-
-  
 int auth(char* logindetails)
 {
-    //The login details are stored in a file login.txt line by line.So we are given logindetails in format username:passwd we open file and
+    //The login details are stored in a file login.txt line by line.So we are given logindetails in format username:password we open file and
     //each line by comparing with logindetails and simultaneoulsy increment the index i for each mismatch.When match found retrn the index i
 
     FILE *fp=fopen("login.txt","r");
@@ -68,10 +331,34 @@ int auth(char* logindetails)
         printf("Login File not exitsing\n");
     }
     if(f) return i;
+
     return 0;
 }   
 int main(int argc , char *argv[])   
 {   
+     for(int i=0;i<1024;i++)
+        {
+            buy[i].item_code=-1;
+            buy[i].user_id=-1;
+            buy[i].price=-1;
+            buy[i].quantity=-1;
+
+            sell[i].item_code=-1;
+            sell[i].user_id=-1;
+            sell[i].price=-1;
+            sell[i].quantity=-1;
+
+        }
+
+         for(int i=0;i<1024;i++)
+        {
+            trade[i].item_code=-1;
+            trade[i].buy_id=-1;
+            trade[i].sell_id=-1;
+            trade[i].price=-1;
+            trade[i].quantity=-1;
+        }
+
     if(argc!=2){
         printf("Number of arguments should be 2\n");
         return 0;
@@ -220,7 +507,8 @@ int main(int argc , char *argv[])
                 }   
             }   
         }   
-             
+        
+       
         //else its some IO operation on some other socket 
         for (int i = 0; i < max_clients; i++)   
         {   
@@ -285,8 +573,10 @@ int main(int argc , char *argv[])
                     }
                     if (t=='B')
                     {
+                        
                     	temp=strtok(NULL,"#");  
                         buy_order(temp,user_id[i]); 
+                        match_trade();
                         char *message2="Message Received\n";
                         write(sd , message2 , strlen(message2) );     
                         break;
@@ -295,8 +585,9 @@ int main(int argc , char *argv[])
                     {
                     	temp=strtok(NULL,"#");  
                         sell_order(temp,user_id[i]); 
-                        char *message2="Message Received\n";
-                        write(sd , message2 , strlen(message2) );     
+                        match_trade();
+                        char *message3="Message Received\n";
+                        write(sd , message3 , strlen(message3) );     
                         break;
                     }
                     if(t=='O')
